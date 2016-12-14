959c0e78e383d47110452965892b70829b2bf449
adminaccount <adminaccount@macminissu2.local>
2016-12-14 09:49:28 +0200

    ballad

--- CalcSashaDudash/CalculatorController.swift
+++ CalcSashaDudash/CalculatorController.swift
@@ -18,7 +18,7 @@ class CalculatorController: UIViewController {
     
     var userIsTypingRightNow = false
     var calculatorBrain = CalculatorBrain()
-    
+    //
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
